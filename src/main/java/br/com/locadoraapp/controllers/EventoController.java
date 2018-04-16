9e2d9df7d1cc393e32eac1a2476e42c74ae634c8
tobiassilva20 <tobiassilva@hotmail.it>
2018-04-16 17:31:24 -0300

    Adicionado exibir eventos

--- src/main/java/br/com/locadoraapp/controllers/EventoController.java
+++ src/main/java/br/com/locadoraapp/controllers/EventoController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.locadoraapp.models.Evento;
 import br.com.locadoraapp.repository.EventoRepository;
@@ -27,5 +28,12 @@ public class EventoController  {
 		er.save(evento);
 		System.out.println("Evento cadastrado com sucesso!");
 		return "redirect:/cadastrarEvento"; 
+	}
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv = new ModelAndView("index");
+		Iterable<Evento> eventos = er.findAll();
+		mv.addObject("eventos", eventos);
+		return mv;
 	}
 }
